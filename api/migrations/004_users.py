473a96e1dbce07e5a40ecffa1d930c8a556190e7
Carlos <carlossoftwareengineer@gmail.com>
2024-02-28 16:43:48 -0800

    Fixed another issue in migration 004

--- api/migrations/004_users.py
+++ api/migrations/004_users.py
@@ -6,40 +6,40 @@ steps = [
             first_name VARCHAR(100) NOT NULL,
             last_name VARCHAR(100) NOT NULL,
             email VARCHAR(200) NOT NULL,
-            phone VARCHAR(12) NOT NULL,
-        );
-        """
-        """
-        DROP TABLE supplier_users;
+            phone VARCHAR(12) NOT NULL
+        )
         """,
-    ]
+        """
+        DROP TABLE supplier_users
+        """
+    ],
     [
         """
         CREATE TABLE employee_users (
             id SERIAL PRIMARY KEY NOT NULL,
-            shop_id INT reference shops(shop_id),
+            shop_id INT REFERENCES shops(shop_id),
             first_name VARCHAR(100) NOT NULL,
             last_name VARCHAR(100) NOT NULL,
             email VARCHAR(200) NOT NULL,
-            phone VARCHAR(12) NOT NULL,
-        );
-        """
-        """
-        DROP TABLE employee_users;
+            phone VARCHAR(12) NOT NULL
+        )
         """,
-    ]
-        [
+        """
+        DROP TABLE employee_users
+        """
+    ],
+    [
         """
         CREATE TABLE customer_users (
             id SERIAL PRIMARY KEY NOT NULL,
             first_name VARCHAR(100) NOT NULL,
             last_name VARCHAR(100) NOT NULL,
             email VARCHAR(200) NOT NULL,
-            phone VARCHAR(12) NOT NULL,
-        );
+            phone VARCHAR(12) NOT NULL
+        )
+        """,
         """
-        """
-        DROP TABLE customer_users;
+        DROP TABLE customer_users
         """
     ]
 ]
