21122b6072b6df3fd4e4dbdad48c0a59d8bf19a7
Stetson <stetson.owings@gmail.com>
2024-02-28 17:32:43 -0700

    finished tables

---
+++ api/migrations/003_shops.py
@@ -0,0 +1,15 @@
+steps = [
+    [
+        """
+        CREATE TABLE shops (
+            id SERIAL PRIMARY KEY NOT NULL,
+            shop_name VARCHAR(100) NOT NULL,
+            address VARCHAR(100) NOT NULL,
+            phone VARCHAR(15) NOT NULL
+        );
+        """,
+        """
+        DROP TABLE shops;
+        """
+    ]
+]
