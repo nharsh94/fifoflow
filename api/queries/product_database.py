84c52b5d6ab52002de51d0ff484156ea94db3225
Carlos <carlossoftwareengineer@gmail.com>
2024-02-29 13:56:08 -0800

    Fixed errors due to spacing and indentions

--- api/queries/product_database.py
+++ api/queries/product_database.py
@@ -5,27 +5,29 @@ from queries.pool import pool
 
 
 class Error(BaseModel):
-    message : str
+    message: str
+
 
 class ProductIn(BaseModel):
-    name : str
-    description : str
-    price : Decimal
+    name: str
+    description: str
+    price: Decimal
     quantity_in_stock: int
-    category : str
-    supplier_id : int
-    alert_threshold : int
+    category: str
+    supplier_id: int
+    alert_threshold: int
 
 
 class ProductOut(BaseModel):
-    id : int
-    name : str
-    description : str
-    price : Decimal
+    id: int
+    name: str
+    description: str
+    price: Decimal
     quantity_in_stock: int
-    category : str
-    supplier_id : int
-    alert_threshold : int
+    category: str
+    supplier_id: int
+    alert_threshold: int
+
 
 class ProductRepository:
 
@@ -65,11 +67,14 @@ class ProductRepository:
             print(e)
             return False
 
-    def update(self, product_id: int, product: ProductIn) -> Union[ProductOut,Error]:
+    def update(
+            self,
+            product_id: int,
+            product: ProductIn) -> Union[ProductOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
-                    result = db.execute(
+                    db.execute(
                         """
                         UPDATE products
                         SET name = %s
@@ -95,13 +100,15 @@ class ProductRepository:
                     return self.product_in_to_out(product_id, product)
         except Exception as e:
             print(e)
-            return {"message" : "Could not update product"}
+            return {"message": "Could not update product"}
 
-    def partial_update(self, product_id: int, product: ProductIn) -> Union[ProductOut,Error]:
+    def partial_update(self,
+                       product_id: int,
+                       product: ProductIn) -> Union[ProductOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
-                    result = db.execute(
+                    db.execute(
                         """
                         UPDATE products
                         SET name = %s
@@ -127,13 +134,13 @@ class ProductRepository:
                     return self.product_in_to_out(product_id, product)
         except Exception as e:
             print(e)
-            return {"message" : "Could not update product"}
+            return {"message": "Could not update product"}
 
     def get_all(self) -> Union[Error, List[ProductOut]]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
-                    result =db.execute(
+                    result = db.execute(
                         """
                         SELECT *
                         FROM products
@@ -145,49 +152,54 @@ class ProductRepository:
                     ]
         except Exception as e:
             print(e)
-            return {"message" : "Could not get all products"}
-
+            return {"message": "Could not get all products"}
 
     def create(self, product: ProductIn) -> ProductOut:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
                     result = db.execute(
-                    """
-                    INSERT INTO products
-                    (name, description, price, quantity_in_stock, category, supplier_id, alert_threshold)
-                    VALUES
-                    (%s, %s, %s, %s, %s, %s,%s)
-                    RETURNING product_id;
-                    """,
-                    [
-                        product.name,
-                        product.description,
-                        product.price,
-                        product.quantity_in_stock,
-                        product.category,
-                        product.supplier_id,
-                        product.alert_threshold
-                    ]
-                )
+                        """
+                        INSERT INTO products
+                        (name,
+                        description,
+                        price,
+                        quantity_in_stock,
+                        category,
+                        supplier_id,
+                        alert_threshold)
+                        VALUES
+                        (%s, %s, %s, %s, %s, %s,%s)
+                        RETURNING product_id;
+                        """,
+                        [
+                            product.name,
+                            product.description,
+                            product.price,
+                            product.quantity_in_stock,
+                            product.category,
+                            product.supplier_id,
+                            product.alert_threshold
+                        ]
+                    )
                 id = result.fetchone()[0]
                 return self.product_in_to_out(id, product)
         except Exception as e:
             print(e)
-            return {"message" : "Could not create product"}
+            return {"message": "Could not create product"}
 
     def product_in_to_out(self, id: int, product: ProductOut):
         old_data = product.dict()
-        return ProductOut(id = id, **old_data)
+        return ProductOut(id=id, **old_data)
 
     def record_to_product_out(self, record):
         return ProductOut(
-            id = record[0],
-            name = record[1],
-            description = record[2],
-            price = record[3],
-            quantity_in_stock = record[4],
-            category = record[5],
-            supplier_id = record[6],
-            alert_threshold = record[7]
+            id=record[0],
+            name=record[1],
+            description=record[2],
+            price=record[3],
+            quantity_in_stock=record[4],
+            category=record[5],
+            supplier_id=record[6],
+            alert_threshold=record[7]
         )
