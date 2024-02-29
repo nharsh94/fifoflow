2a1b5a547a00667e5c945658282516589a9be7a4
Stetson <stetson.owings@gmail.com>
2024-02-28 18:19:50 -0700

    fixed an issue with the tables

--- api/migrations/002_users.py
+++ api/migrations/002_users.py
@@ -2,42 +2,42 @@ steps = [
     [
         """
         CREATE TABLE supplier_users (
-            id SERIAL PRIMARY KEY NOT NULL,
+            supplier_id SERIAL PRIMARY KEY NOT NULL,
             first_name VARCHAR(100) NOT NULL,
             last_name VARCHAR(100) NOT NULL,
             email VARCHAR(200) NOT NULL,
-            phone VARCHAR(12) NOT NULL,
+            phone VARCHAR(12) NOT NULL
         );
-        """
+        """,
         """
         DROP TABLE supplier_users;
-        """,
-    ]
+        """
+    ],
     [
         """
         CREATE TABLE employee_users (
-            id SERIAL PRIMARY KEY NOT NULL,
-            shop_id INT reference shops(shop_id),
+            employee_id SERIAL PRIMARY KEY NOT NULL,
+            shop_id INT references shops(shop_id),
             first_name VARCHAR(100) NOT NULL,
             last_name VARCHAR(100) NOT NULL,
             email VARCHAR(200) NOT NULL,
-            phone VARCHAR(12) NOT NULL,
+            phone VARCHAR(12) NOT NULL
         );
-        """
+        """,
         """
         DROP TABLE employee_users;
-        """,
-    ]
-        [
+        """
+    ],
+    [
         """
         CREATE TABLE customer_users (
-            id SERIAL PRIMARY KEY NOT NULL,
+            customer_id SERIAL PRIMARY KEY NOT NULL,
             first_name VARCHAR(100) NOT NULL,
             last_name VARCHAR(100) NOT NULL,
             email VARCHAR(200) NOT NULL,
-            phone VARCHAR(12) NOT NULL,
+            phone VARCHAR(12) NOT NULL
         );
-        """
+        """,
         """
         DROP TABLE customer_users;
         """
---
+++ api/migrations/004_orders.py
@@ -0,0 +1,36 @@
+steps = [
+        [
+        """
+        CREATE TABLE orders (
+            order_id SERIAL PRIMARY KEY,
+            shop_id INT REFERENCES shops(shop_id),
+            employee_id INT references employee_users(employee_id),
+            customer_id INT references customer_users(customer_id),
+            order_date TIMESTAMP,
+            order_item INT,
+            product_id INT references products(product_id),
+            quantity INT,
+            total_price DECIMAL(10, 2)
+        );
+        """,
+        """
+        DROP TABLE orders;
+        """
+    ],
+    [
+        """
+        CREATE TABLE order_items (
+            id SERIAL PRIMARY KEY NOT NULL,
+            shop_id INT references shops(shop_id) NOT NULL,
+            order_id INT references orders(order_id) NOT NULL,
+            product_id INT references products(product_id) NOT NULL,
+            quantity INT NOT NULL,
+            unit_price DECIMAL(10, 2) NOT NULL,
+            total_price DECIMAL(10, 2) NOT NULL
+        );
+        """,
+        """
+        DROP TABLE order_items;
+        """
+    ]
+]
