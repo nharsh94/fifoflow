0e1bff9e12c92a9e43722151654ea464e5e081eb
Carlos <carlossoftwareengineer@gmail.com>
2024-02-29 18:30:20 -0800

    Refractor code to into models folder

---
+++ api/models/products.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from decimal import Decimal
+
+
+class Error(BaseModel):
+    message: str
+
+
+class ProductIn(BaseModel):
+    name: str
+    description: str
+    price: Decimal
+    quantity_in_stock: int
+    category: str
+    supplier_id: int
+    alert_threshold: int
+
+
+class ProductOut(BaseModel):
+    id: int
+    name: str
+    description: str
+    price: Decimal
+    quantity_in_stock: int
+    category: str
+    supplier_id: int
+    alert_threshold: int
--- api/routers/products.py
+++ api/routers/products.py
@@ -1,18 +1,16 @@
 from fastapi import APIRouter, Depends, Response
-from queries.product_database import (ProductIn,
-                                      ProductRepository,
-                                      ProductOut,
-                                      Error)
+from queries.product_database import ProductRepository
 from typing import List, Union, Optional
+import models.products as pro
 
 router = APIRouter()
 
 
 @router.post("/products",
-             response_model=Union[ProductOut, Error],
+             response_model=Union[pro.ProductOut, pro.Error],
              tags=["Products"])
 def create_products(
-    product: ProductIn,
+    product: pro.ProductIn,
     response: Response,
     repo: ProductRepository = Depends()
 ):
@@ -21,47 +19,48 @@ def create_products(
 
 
 @router.get("/products",
-            response_model=Union[List[ProductOut], Error],
+            response_model=Union[List[pro.ProductOut], pro.Error],
             tags=["Products"])
 def get_all(repo: ProductRepository = Depends()):
     return repo.get_all()
 
 
-@router.put("products/{product_id}",
-            response_model=Union[ProductOut, Error],
+@router.put("/products/{product_id}",
+            response_model=Union[pro.ProductOut, pro.Error],
             tags=["Products"])
 def update_product(product_id: int,
-                   product: ProductIn,
-                   repo: ProductRepository = Depends()) -> Union[Error,
-                                                                 ProductOut]:
+                   product: pro.ProductIn,
+                   repo: ProductRepository = Depends()
+                   ) -> Union[pro.ProductOut, pro.Error]:
     return repo.update(product_id, product)
 
 
-@router.delete("products/{product_id}", response_model=bool, tags=["Products"])
+@router.delete("/products/{product_id}",
+               response_model=bool, tags=["Products"])
 def delete_product(product_id: int,
                    repo: ProductRepository = Depends()) -> bool:
     return repo.delete(product_id)
 
 
-@router.get("products/{product_id}",
-            response_model=Optional[ProductOut],
+@router.get("/products/{product_id}",
+            response_model=Optional[pro.ProductOut],
             tags=["Products"])
 def get_one_product(product_id: int,
                     response: Response,
-                    repo: ProductRepository = Depends()) -> ProductOut:
+                    repo: ProductRepository = Depends()) -> pro.ProductOut:
     product = repo.get_one(product_id)
     if product in None:
         response.status_code = 404
     return product
 
 
-@router.patch("products/{product_id}",
-              response_model=Union[ProductOut, Error],
+@router.patch("/products/{product_id}",
+              response_model=Union[pro.ProductOut, pro.Error],
               tags=["Products"])
 def pratial_update_product(product_id: int,
-                           product: ProductIn,
+                           product: pro.ProductIn,
                            repo: ProductRepository = Depends()
-                           ) -> Union[Error, ProductOut]:
+                           ) -> Union[pro.ProductOut, pro.Error]:
     return repo.partial_update(
         product_id,
         product
