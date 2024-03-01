dd5ca9b57607f4941d9eb7ecde0f76691b7c0a3d
Stetson <stetson.owings@gmail.com>
2024-03-01 16:51:53 -0700

    fixing flake errors

--- api/main.py
+++ api/main.py
@@ -35,36 +35,6 @@ app.add_middleware(
 )
 
 
-@app.post("/api/user")
-def create_user(
-    info: UserRequest,
-    users: UserQueries = Depends(),
-) -> UserResponse:
-    hashed_password = authenticator.hash_password(info.password)
-    ar = User(
-        username=info.username,
-        first_name=info.first_name,
-        last_name=info.last_name,
-        email=info.email,
-    )
-    try:
-        pk = users.create_user(ar, hashed_password)
-    except Exception as e:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=str(e),
-        )
-    user = users.get_user_by_id(pk)
-    return UserResponse(
-        id=user.id,
-        username=user.username,
-        first_name=user.first_name,
-        last_name=user.last_name,
-        email=user.email,
-        modified=user.modified.isoformat(),
-    )
-
-
 @app.get("/api/launch-details")
 def launch_details():
     return {
