c6ddd9d91d3a931252213829ec1f73dd6f89f748
Manuel Kazaka <m.kazaka@gmail.com>
2024-03-05 12:37:59 -0800

    Fixed Polling

--- ghi/vite.config.js
+++ ghi/vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react({include: '**/*.jsx'})],
     server: {
+        hmr: true,
         host: true,
         strictPort: true,
+        watch: {
+            usePolling: true,
+        }
     },
 })
