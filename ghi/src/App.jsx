7904240624acb6711f62727967d181177349ad11
Manuel Kazaka <m.kazaka@gmail.com>
2024-03-08 19:16:56 -0800

    Shop Form and List created. Nav Created.

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,16 +1,22 @@
 // This makes VSCode check types as if you are using TypeScript
 //@ts-check
-import { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ErrorNotification from './ErrorNotification'
-import Construct from './Construct'
-import './App.css'
-import React from 'react'
-import UserPage from './UserPage'
-import SignUpForm from './SignUpForm'
-import ForgotPassword from './ForgotPassword'
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ErrorNotification from './ErrorNotification';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Nav from './Nav';
+import Construct from './Construct';
+import UserPage from './UserPage';
+import SignUpForm from './SignUpForm';
+import ForgotPassword from './ForgotPassword';
+import ShopCreate from './ShopCreate';
+import ShopsList from './ShopsList';
+import ShopDetails from './ShopDetails';
+
 
 // All your environment variables in vite are in this object
 console.table(import.meta.env)
@@ -23,17 +29,7 @@ if (!API_HOST) {
     throw new Error('VITE_API_HOST is not defined')
 }
 
-/**
- * This is an example of using JSDOC to define types for your component
- * @typedef {{module: number, week: number, day: number, min: number, hour: number}} LaunchInfo
- * @typedef {{launch_details: LaunchInfo, message?: string}} LaunchData
- *
- * @returns {React.ReactNode}
- */
 function App() {
-    // Replace this App component with your own.
-    /** @type {[LaunchInfo | undefined, (info: LaunchInfo) => void]} */
-    const [launchInfo, setLaunchInfo] = useState()
     const [error, setError] = useState(null)
 
     useEffect(() => {
@@ -41,20 +37,12 @@ function App() {
             let url = `${API_HOST}/api/launch-details`
             console.log('fastapi url: ', url)
             let response = await fetch(url)
-            /** @type {LaunchData} */
-            let data = await response.json()
 
             if (response.ok) {
-                if (!data.launch_details) {
-                    console.log('drat! no launch data')
-                    setError('No launch data')
-                    return
-                }
-                console.log('got launch data!')
-                setLaunchInfo(data.launch_details)
+                    console.log('Data fetched successfully!');
             } else {
-                console.log('drat! something happened')
-                setError(data.message)
+                    console.log('Error fetching data');
+                    setError('No fetched data')
             }
         }
         getData()
@@ -63,11 +51,18 @@ function App() {
     return (
         <BrowserRouter>
             <div className="App">
+                <Nav />
                 <ErrorNotification error={error} />
                 <Routes>
-                    <Route path="/" element={<Construct info={launchInfo} />} />
+                    <Route path="/" element={<Construct info={{}} />} />
+
                     <Route path="/user" element={<UserPage />} />
                     <Route path="/signup" element={<SignUpForm />} />
+                    <Route path="/shops">
+                        <Route path="create" element={<ShopCreate />} />
+                        <Route path="list" element={<ShopsList />} />
+                        <Route path="details" element={<ShopDetails shopId={123} />} />
+                    </Route>
                     <Route
                         path="/forgot-password"
                         element={<ForgotPassword />}
@@ -78,4 +73,4 @@ function App() {
         </BrowserRouter>
     )
 }
-export default App
+export default App;
---
+++ ghi/src/ShopsList.jsx
@@ -0,0 +1,195 @@
+import { useEffect, useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Form from 'react-bootstrap/Form';
+import Table from 'react-bootstrap/Table';
+
+function ShopsList() {
+    const [shops, setShop] = useState([]);
+    const [selectedShop, setSelectedShop] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+
+    const getData = async () => {
+            try {
+                const response = await fetch('http://localhost:8000/api/shops');
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setShop(data)
+                } else {
+                    throw new Error(
+                        'Failed to fetch data. Status: ${response.status}'
+                    )
+                }
+            } catch (error) {
+                console.error('Error fetching shop data', error);
+            }
+        };
+
+    useEffect(() => {
+        getData()
+    }, []);
+
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    const handleShowModal = async (shop) => {
+        try {
+            const response = await fetch(`http://localhost:8000/api/shops/${shop.shop_id}/`);
+            if (response.ok) {
+                const data = await response.json();
+                setSelectedShop(data);
+                setShowModal(true);
+            } else {
+                throw new Error(`Failed to fetch shop details. Status: ${response.status}`);
+            }
+        } catch (error) {
+            console.error('Error fetching shop details', error);
+        }
+    }
+
+    const handleUpdateShop = async () => {
+        try {
+            const response = await fetch(`http://localhost:8000/api/shops/${selectedShop.shop_id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(selectedShop),
+            });
+
+            if (response.ok) {
+                console.log('Shop updated successfully:', selectedShop);
+                handleCloseModal();
+                getData();
+            } else {
+                throw new Error(`Failed to update shop. Status: ${response.status}`);
+            }
+        } catch (error) {
+            console.error('Error updating shop', error);
+        }
+    };
+
+    const handleDeleteShop = async () => {
+        const confirmDelete = window.confirm('Are you sure you want to delete this shop?');
+        if (confirmDelete) {
+            try {
+                const response = await fetch(`http://localhost:8000/api/shops/${selectedShop.shop_id}`, {
+                    method: 'DELETE',
+                });
+
+                if (response.ok) {
+                    console.log('Shop deleted successfully', selectedShop);
+                    getData();
+                    handleCloseModal();
+                } else {
+                    throw new Error(`Failed to delete shop. Status: ${response.status}`);
+                }
+            } catch (error) {
+                console.error('Error deleting shop', error);
+            }
+        }
+
+    };
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setSelectedShop((prevShop) => ({ ...prevShop, [name]: value }));
+    };
+
+    return (
+        <>
+            <div>
+                <h1>Shops</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Shop ID</th>
+                            <th>Name</th>
+                            <th>Address</th>
+                            <th>Phone</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {shops.map((shop) => {
+                            return (
+                                <tr key={shop.shop_id}>
+                                    <td>{shop.shop_id}</td>
+                                    <td>{shop.shop_name}</td>
+                                    <td>{shop.address}</td>
+                                    <td>{shop.phone}</td>
+                                    <td>
+                                        <Button
+                                            variant="primary"
+                                            onClick={() =>
+                                                handleShowModal(shop)
+                                            }
+                                        >
+                                            Edit
+                                        </Button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
+            </div>
+
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Shop</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formShopName">
+                            <Form.Label>Shop Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter shop name"
+                                name="shop_name"
+                                value={selectedShop?.shop_name || ''}
+                                onChange={handleInputChange}
+                            />
+                            </Form.Group>
+                        <Form.Group controlId="formAddress">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter address"
+                                name="address"
+                                value={selectedShop?.address || ''}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formPhone">
+                            <Form.Label>Phone</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter phone"
+                                name="phone"
+                                value={selectedShop?.phone || ''}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleUpdateShop}>
+                        Save Changes
+                    </Button>
+                    <Button variant="danger" onClick={handleDeleteShop}>
+                        Delete Shop
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
+}
+
+export default ShopsList;
