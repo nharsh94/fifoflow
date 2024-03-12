28b9bb18d65302a47ea15f7a6a6ed2d0b54e62ef
Stetson <stetson.owings@gmail.com>
2024-03-12 09:08:24 -0700

    order db update

--- api/migrations/004_orders.py
+++ api/migrations/004_orders.py
@@ -9,11 +9,11 @@ steps = [
         """
         CREATE TABLE orders (
             order_id SERIAL PRIMARY KEY,
-            FOREIGN KEY (shop_id) INT REFERENCES shops(shop_id),
-            FOREIGN KEY (role_id) INT REFERENCES profiles(role_id),
+            FOREIGN KEY (shop_id) REFERENCES shops(shop_id),
+            FOREIGN KEY (role_id) REFERENCES profiles(role_id),
             order_date TIMESTAMP,
             order_item INT,
-            FOREIGN KEY (product_id) INT REFERENCES products(product_id),
+            FOREIGN KEY (product_id) REFERENCES products(product_id),
             quantity INT,
             total_price DECIMAL(10, 2)
             status status_type
@@ -27,13 +27,13 @@ steps = [
         """
         CREATE TABLE order_items (
             id SERIAL PRIMARY KEY NOT NULL,
-            FOREIGN KEY (shop_id) INT REFERENCES shops(shop_id) NOT NULL,
-            FOREIGN KEY (order_id) INT REFERENCES orders(order_id) NOT NULL,
-            FOREIGN KEY (product_id) INT REFERENCES products(product_id) NOT NULL,
+            FOREIGN KEY (shop_id) REFERENCES shops(shop_id) NOT NULL,
+            FOREIGN KEY (order_id) REFERENCES orders(order_id) NOT NULL,
+            FOREIGN KEY (product_id) REFERENCES products(product_id) NOT NULL,
             quantity INT NOT NULL,
             unit_price DECIMAL(10, 2) NOT NULL,
             total_price DECIMAL(10, 2) NOT NULL,
-            FOREIGN KEY (status) status_type REFERENCES orders(status)
+            FOREIGN KEY (status) REFERENCES orders(status)
         );
         """,
         """
