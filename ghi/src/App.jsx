24776258d4a25dd4dff6ebdf5d99c60320a2505f
Manuel Kazaka <m.kazaka@gmail.com>
2024-03-13 17:01:09 +0000

    Fixed up frontend webpages with Bootstrap.

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -1,30 +1,40 @@
+// This makes VSCode check types as if you are using TypeScript
+//@ts-check
 import React from 'react'
 import { useState, useEffect } from 'react'
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
-import Header from './Header'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import ErrorNotification from './ErrorNotification'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
-import CreateProduct from './CreateProduct'
-import ProductsList from './ProductsList'
 import Nav from './Nav'
 import Construct from './Construct'
 import UserPage from './UserPage'
 import SignUpForm from './SignUpForm'
 import ForgotPassword from './ForgotPassword'
+
 import ShopCreate from './ShopCreate'
 import ShopsList from './ShopsList'
 
+import CreateProduct from './CreateProduct'
+import ProductsList from './ProductsList'
+
+import OrderList from './list_orders'
+//
+import TestProductCreate from './TestProductCreate' // By Mel K
+import TestProductsList from './TestProductsList'  // By Mel K
+// import ProductDetails from './ProductDetails'
+//
+
+// import ThreeScene from './ThreeScene'
+
+
+// All your environment variables in vite are in this object
 console.table(import.meta.env)
 const API_HOST = import.meta.env.VITE_API_HOST
 
 if (!API_HOST) {
     throw new Error('VITE_API_HOST is not defined')
-}
-
-function Navigation() {
-    const location = useLocation()
-    return location.pathname !== '/' && <Nav />
 }
 
 function App() {
@@ -49,25 +59,34 @@ function App() {
     return (
         <BrowserRouter>
             <div className="App">
-                <Header />
-                <Navigation />
+                <Nav />
                 <ErrorNotification error={error} />
                 <Routes>
                     <Route path="/" element={<Construct info={{}} />} />
 
+                    {/* <Route path="/threescene" element={<ThreeScene />} /> */}
+
                     <Route path="/user" element={<UserPage />} />
                     <Route path="/signup" element={<SignUpForm />} />
-                    <Route path="/shops">
-                        <Route path="create" element={<ShopCreate />} />
-                        <Route path="list" element={<ShopsList />} />
-                    </Route>
                     <Route
                         path="/forgot-password"
                         element={<ForgotPassword />}
                     />
+
+                    <Route path="/shops">
+                        <Route path="create" element={<ShopCreate />} />
+                        <Route path="list" element={<ShopsList />} />
+                        {/* <Route path="details" element={<ShopDetails />} /> */}
+                    </Route>
+
                     <Route path="/products">
                         <Route path="create" element={<CreateProduct />} />
                         <Route path="list" element={<ProductsList />} />
+                        <Route path="create1" element={<TestProductCreate />} />
+                        <Route path="list1" element={<TestProductsList />} />
+                        {/* <Route path="details" element={<ProductDetails />} /> */}
+
+                        <Route path="/orders" element={<OrderList />} />
                     </Route>
                 </Routes>
             </div>
---
+++ ghi/src/TestProductsList.jsx
@@ -0,0 +1,245 @@
+import { useEffect, useState } from 'react'
+
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
+import Form from 'react-bootstrap/Form'
+import Table from 'react-bootstrap/Table'
+
+function TestProductsList() {
+    const [products, setProducts] = useState([])
+    const [selectedProduct, setSelectedProduct] = useState(null)
+    const [showModal, setShowModal] = useState(false)
+
+    const getData = async () => {
+        try {
+            const response = await fetch('http://localhost:8000/api/products')
+
+            if (response.ok) {
+                const data = await response.json()
+                setProducts(data)
+            } else {
+                throw new Error(
+                    'Failed to fetch data. Status: ${response.status}'
+                )
+            }
+        } catch (error) {
+            console.error('Error fetching product data', error)
+        }
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
+
+    const handleCloseModal = () => {
+        setShowModal(false)
+    }
+
+    const handleShowModal = async (product) => {
+        try {
+            const response = await fetch(
+                `http://localhost:8000/api/products/${product.product_id}`
+            )
+            if (response.ok) {
+                const data = await response.json()
+                setSelectedProduct(data)
+                setShowModal(true)
+            } else {
+                throw new Error(
+                    `Failed to fetch product details. Status: ${response.status}`
+                )
+            }
+        } catch (error) {
+            console.error('Error fetching product details', error)
+        }
+    }
+
+    const handleUpdateProduct = async () => {
+        try {
+            const response = await fetch(
+                `http://localhost:8000/api/products/${selectedProduct.product_id}`,
+                {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(selectedProduct),
+                }
+            )
+
+            if (response.ok) {
+                console.log('Product updated successfully:', selectedProduct)
+                handleCloseModal()
+                getData()
+            } else {
+                throw new Error(
+                    `Failed to update product. Status: ${response.status}`
+                )
+            }
+        } catch (error) {
+            console.error('Error updating product', error)
+        }
+    }
+
+    const handleDeleteProduct = async () => {
+        const confirmDelete = window.confirm(
+            'Are you sure you want to delete this Product?'
+        )
+        if (confirmDelete) {
+            try {
+                const response = await fetch(
+                    `http://localhost:8000/api/products/${selectedProduct.product_id}`,
+                    {
+                        method: 'DELETE',
+                    }
+                )
+
+                if (response.ok) {
+                    console.log('Product deleted successfully', selectedProduct)
+                    getData()
+                    handleCloseModal()
+                } else {
+                    throw new Error(
+                        `Failed to delete product. Status: ${response.status}`
+                    )
+                }
+            } catch (error) {
+                console.error('Error deleting product', error)
+            }
+        }
+    }
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+        setSelectedProduct((prevProduct) => ({ ...prevProduct, [name]: value }))
+    }
+
+    return (
+        <>
+            <div>
+                <h1>Products in Flow</h1>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Product ID</th>
+                            <th>Name</th>
+                            <th>Description</th>
+                            <th>Price</th>
+                            <th>Quantity in Stock</th>
+                            <th>Category</th>
+                            <th>Shop Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products.map((product) => {
+                            return (
+                                <tr key={product.product_id}>
+                                    <td>{product.product_id}</td>
+                                    <td>{product.name}</td>
+                                    <td>{product.description}</td>
+                                    <td>{product.price}</td>
+                                    <td>{product.quantity_in_stock}</td>
+                                    <td>{product.category}</td>
+                                    <td>{product.shop_name}</td>
+                                    <td>
+                                        <Button
+                                            variant="primary"
+                                            onClick={() =>
+                                                handleShowModal(product)
+                                            }
+                                        >
+                                            Edit
+                                        </Button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </Table>
+            </div>
+
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Edit Product</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form>
+                        <Form.Group controlId="formProductName">
+                            <Form.Label>Product Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter product name"
+                                name="name"
+                                value={selectedProduct?.name || ''}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formDescription">
+                            <Form.Label>Description</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter Description"
+                                name="description"
+                                value={selectedProduct?.description || ''}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formPrice">
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="Enter phone"
+                                name="price"
+                                value={selectedProduct?.price || ''}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formQIS">
+                            <Form.Label>Quantity in Stock</Form.Label>
+                            <Form.Control
+                                type="number"
+                                placeholder="Enter quantity"
+                                name="quantity_in_stock"
+                                value={selectedProduct?.quantity_in_stock || ''}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formCategory">
+                            <Form.Label>Category</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter category"
+                                name="category"
+                                value={selectedProduct?.category || ''}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="formShopName">
+                            <Form.Label>Shop Name</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter shop name"
+                                name="shop_name"
+                                value={selectedProduct?.shop_name || ''}
+                                onChange={handleInputChange}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleUpdateProduct}>
+                        Save Changes
+                    </Button>
+                    <Button variant="danger" onClick={handleDeleteProduct}>
+                        Delete Product
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default TestProductsList
