9173a6f12f835230869e23f131bf393c59e4ea09
Noah Harshbarger <noahharshb@gmail.com>
2024-03-13 18:16:30 -0600

    Fixed coding bugs across ft1/2, all components work

--- ghi/src/Nav.jsx
+++ ghi/src/Nav.jsx
@@ -21,15 +21,22 @@ function Navs() {
                                     Inventory
                                 </Nav.Link>
                             </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link
-                                    className="px-3"
+                            <NavDropdown
+                                className="px-2"
+                                title="Orders"
+                                id="nav-dropdown"
+                            >
+                                <NavDropdown.Item as={NavLink} to="/orders">
+                                    All Orders
+                                </NavDropdown.Item>
+                                <NavDropdown.Item
                                     as={NavLink}
-                                    to="/api/orders"
+                                    to="/create-order"
                                 >
-                                    Ordering
-                                </Nav.Link>
-                            </Nav.Item>
+                                    Create an Order
+                                </NavDropdown.Item>
+                            </NavDropdown>
+
                             <NavDropdown
                                 className="px-2"
                                 title="Products"
