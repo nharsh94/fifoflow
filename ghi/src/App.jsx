258f5ea67a1b711d967a954bfd1160113ee98ea9
Noah Harshbarger <noahharshb@gmail.com>
2024-03-13 18:41:33 -0600

    turned on toastify, updated App

--- ghi/src/App.jsx
+++ ghi/src/App.jsx
@@ -63,40 +63,49 @@ function App() {
 
     return (
         <BrowserRouter>
-            <div className="App">
-                <Header />
-                <Nav />
-                <ErrorNotification error={error} />
-                <Routes>
-                    <Route path="/" element={<Construct info={{}} />} />
-                    <Route path="/orders" element={<OrderList />} />
-                    <Route path="/create-order" element={<OrderCreate />} />
-                    <Route path="/user" element={<UserPage />} />
-                    <Route path="/signup" element={<SignUpForm />} />
-                    <Route path="/shops">
-                        <Route path="create" element={<ShopCreate />} />
-                        <Route path="list" element={<ShopsList />} />
-                        {/* <Route path="details" element={<ShopDetails shopId={123} />} /> */}
-                    </Route>
-                    {/* <Route path="/threescene" element={<ThreeScene />} /> */}
+            <UserProvider>
+                <div className="App">
+                    <Header />
+                    <Navigation />
+                    <ErrorNotification error={error} />
+                    <Routes>
+                        <Route path="/" element={<Construct info={{}} />} />
+                        <Route path="/orders" element={<OrderList />} />
+                        <Route path="/create-order" element={<OrderCreate />} />
+                        <Route path="/user" element={<UserPage />} />
+                        <Route path="/signup" element={<SignUpUserForm />} />
+                        <Route path="/role" element={<AssignRole />} />
+                        <Route path="/profile" element={<CreateProfile />} />
+                        <Route path="/shops">
+                            <Route path="create" element={<ShopCreate />} />
+                            <Route path="list" element={<ShopsList />} />
+                            {/* <Route path="details" element={<ShopDetails shopId={123} />} /> */}
+                        </Route>
+                        {/* <Route path="/threescene" element={<ThreeScene />} /> */}
 
-                    <Route path="/user" element={<UserPage />} />
-                    <Route path="/signup" element={<SignUpForm />} />
-                    <Route
-                        path="/forgot-password"
-                        element={<ForgotPassword />}
-                    />
+                        <Route path="/user" element={<UserPage />} />
+                        <Route
+                            path="/forgot-password"
+                            element={<ForgotPassword />}
+                        />
 
-                    <Route path="/products">
-                        <Route path="create" element={<CreateProduct />} />
-                        <Route path="list" element={<ProductsList />} />
-                        <Route path="create1" element={<TestProductCreate />} />
-                        <Route path="list1" element={<TestProductsList />} />
-                        {/* <Route path="details" element={<ProductDetails />} /> */}
-                    </Route>
-                    {/* Define more routes as needed */}
-                </Routes>
-            </div>
+                        <Route path="/products">
+                            <Route path="create" element={<CreateProduct />} />
+                            <Route path="list" element={<ProductsList />} />
+                            <Route
+                                path="create1"
+                                element={<TestProductCreate />}
+                            />
+                            <Route
+                                path="list1"
+                                element={<TestProductsList />}
+                            />
+                            {/* <Route path="details" element={<ProductDetails />} /> */}
+                        </Route>
+                        {/* Define more routes as needed */}
+                    </Routes>
+                </div>
+            </UserProvider>
         </BrowserRouter>
     )
 }
