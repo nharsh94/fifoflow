1957d0a2503e4d16f0fee24d9f8390d71fe50124
Stetson <stetson.owings@gmail.com>
2024-03-18 15:56:25 -0700

    comiiting so i can pull

--- ghi/src/list_orders.jsx
+++ ghi/src/list_orders.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import SearchComponent from './Search'
 
 function OrderList() {
     const [orders, setOrders] = useState([])
     const [products, setProducts] = useState([])
     const [shops, setShops] = useState([])
     const [users, setUsers] = useState([])
+    const [searchQuery, setSearchQuery] = useState([])
 
     const getOrderData = async () => {
         const response = await fetch('http://localhost:8000/api/orders')
@@ -63,62 +65,93 @@ function OrderList() {
 
         // location.reload()
     }
-
+    const handleSearch = (e) => {
+        const inputValue = e.target.value.toLowerCase()
+        if (typeof inputValue === 'string') {
+            setSearchQuery(inputValue)
+        }
+    }
+    const filteredOrders = orders.filter((order) => {
+        const product = products.find(
+            (product) => product.product_id === order.product_id
+        )
+        if (product) {
+            return product.name.toLowerCase().includes(searchQuery)
+        }
+        return false
+    })
     return (
-        <table>
-            <thead>
-                <tr>
-                    <th>Item</th>
-                    <th>Quantity</th>
-                    <th>Shop</th>
-                    <th>Total Price</th>
-                    <th>Orderer</th>
-                    <th>Order Date</th>
-                    <th>Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                {orders.map((order) => {
-                    const product = products.find(
-                        (product) => product.product_id === order.product_id
-                    )
-                    const shop = shops.find(
-                        (shop) => shop.shop_id === order.shop_id
-                    )
-                    const user = users.find(
-                        (user) => user.user_id === order.user_id
-                    )
-                    const date = new Date(order.order_date)
-                    const formattedDate = `${
-                        date.getMonth() + 1
-                    }-${date.getDate()}-${date.getFullYear()}`
-                    return (
-                        <tr key={order.order_id}>
-                            <td>
-                                {product ? product.name : 'Product not found'}
-                            </td>
-                            <td>{order.quantity}</td>
-                            <td>{shop ? shop.shop_name : 'Shop not found'}</td>
-                            <td>{order.total_price}</td>
-                            <td>
-                                {user
-                                    ? user.first_name + ' ' + user.last_name
-                                    : 'User not found'}
-                            </td>
-                            <td>{formattedDate}</td>
-                            <td>{order.status}</td>
-                            <td>
-                                <button
-                                    onClick={() => handleCancel(order.order_id)}
-                                >
-                                    cancel
-                                </button>
-                            </td>
+        <>
+            <div>
+                <h1>Orders</h1>
+                <SearchComponent value={searchQuery} onChange={handleSearch} />
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Product</th>
+                            <th>Quantity</th>
+                            <th>Shop</th>
+                            <th>Total Price</th>
+                            <th>Orderer</th>
+                            <th>Order Date</th>
+                            <th>Status</th>
                         </tr>
-                    )
-                })}
-            </tbody>
-        </table>
+                    </thead>
+                    <tbody>
+                        {filteredOrders.map((order) => {
+                            const product = products.find(
+                                (product) =>
+                                    product.product_id === order.product_id
+                            )
+                            const shop = shops.find(
+                                (shop) => shop.shop_id === order.shop_id
+                            )
+                            const user = users.find(
+                                (user) => user.user_id === order.user_id
+                            )
+                            const date = new Date(order.order_date)
+                            const formattedDate = `${
+                                date.getMonth() + 1
+                            }-${date.getDate()}-${date.getFullYear()}`
+                            return (
+                                <tr key={order.order_id}>
+                                    <td>
+                                        {product
+                                            ? product.name
+                                            : 'Product not found'}
+                                    </td>
+                                    <td>{order.quantity}</td>
+                                    <td>
+                                        {shop
+                                            ? shop.shop_name
+                                            : 'Shop not found'}
+                                    </td>
+                                    <td>{order.total_price}</td>
+                                    <td>
+                                        {user
+                                            ? user.first_name +
+                                              ' ' +
+                                              user.last_name
+                                            : 'User not found'}
+                                    </td>
+                                    <td>{formattedDate}</td>
+                                    <td>{order.status}</td>
+                                    <td>
+                                        <button
+                                            onClick={() =>
+                                                handleCancel(order.order_id)
+                                            }
+                                        >
+                                            cancel
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </>
     )
 }
 
