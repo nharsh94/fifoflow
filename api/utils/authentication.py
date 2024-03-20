39c313a5b5d6ee72576a7b68c1502e6a4143ce3c
Noah Harshbarger <noahharshb@gmail.com>
2024-03-20 15:42:20 -0600

    cleaned up code

--- api/utils/authentication.py
+++ api/utils/authentication.py
@@ -9,19 +9,14 @@ from typing import Annotated, Optional
 from models.jwt import JWTPayload, JWTUserData
 from queries.user_database import UserWithPw
 
-# If you ever need to change the hashing algorithm, you can change it here
 ALGORITHM = ALGORITHMS.HS256
 
-# We pull this from the environment
 SIGNING_KEY = os.environ.get("SIGNING_KEY")
 if not SIGNING_KEY:
     raise ValueError("SIGNING_KEY environment variable not set")
 
 
 async def decode_jwt(token: str) -> Optional[JWTPayload]:
-    """
-    Helper function to decode the JWT from a token string
-    """
     try:
         payload = jwt.decode(token, SIGNING_KEY, algorithms=[ALGORITHM])
         return JWTPayload(**payload)
@@ -33,14 +28,8 @@ async def decode_jwt(token: str) -> Optional[JWTPayload]:
 async def try_get_jwt_user_data(
     fast_api_token: Annotated[str | None, Cookie()] = None,
 ) -> tuple[Optional[JWTUserData], Optional[str]]:
-    """
-    This function attempts to decode the JWT token from the provided FastAPI cookie.
-    It returns the decoded JWT payload (if valid) along with the cookie value.
-    """
     if not fast_api_token:
         return None, None
-
-    # Decode the JWT token
     payload = await decode_jwt(fast_api_token)
     if not payload:
         return None, None
@@ -49,20 +38,12 @@ async def try_get_jwt_user_data(
 
 
 def verify_password(plain_password, hashed_password) -> bool:
-    """
-    This verifies the user's password, by hashing the plain
-    password and then comparing it to the hashed password
-    from the database
-    """
     return bcrypt.checkpw(
         plain_password.encode("utf-8"), hashed_password.encode("utf-8")
     )
 
 
 def hash_password(plain_password) -> str:
-    """
-    Helper function that hashes a password
-    """
     return bcrypt.hashpw(
         plain_password.encode("utf-8"), bcrypt.gensalt()
     ).decode()
@@ -71,10 +52,6 @@ def hash_password(plain_password) -> str:
 def generate_jwt(
     user: UserWithPw, expires_at: Optional[datetime] = None
 ) -> str:
-    """
-    Generates a new JWT token using the user's information
-    Optionally accepts an expiration time for the token
-    """
     exp = expires_at or datetime.utcnow() + timedelta(hours=1)
     jwt_data = JWTPayload(
         exp=timegm(exp.utctimetuple()),
