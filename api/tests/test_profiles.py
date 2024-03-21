5b85c13f53437fcd2f8576b74bdc2aac051c3967
Carlos <carlossoftwareengineer@gmail.com>
2024-03-21 10:20:36 -0700

    Implemented unit test

---
+++ api/tests/test_profiles.py
@@ -0,0 +1,204 @@
+from fastapi.testclient import TestClient
+from main import app
+from models.profiles import ProfileOut, ProfileIn
+from queries.profile_database import ProfileRepository
+
+
+client = TestClient(app)
+
+
+class ProfileQuieres:
+    def get_all(self):
+        return [
+            ProfileOut(
+                id=1,
+                user_id=1,
+                role="Admin",
+                first_name="string",
+                last_name="string",
+                email="string@example.com",
+                phone="1234567890",
+            ),
+            ProfileOut(
+                id=2,
+                user_id=2,
+                role="Supplier",
+                first_name="string2",
+                last_name="string2",
+                email="string2@example.com",
+                phone="0987654321",
+            ),
+        ]
+
+    def get_one(self, user_id: int):
+        profiles = self.get_all()
+        for profile in profiles:
+            if profile.user_id == user_id:
+                return profile
+        return None
+
+
+class CreateQuieres:
+    def create(self, profile: ProfileIn):
+        result = {
+            "id": 1,
+            "user_id": 1,
+            "role": "string",
+            "first_name": "string",
+            "last_name": "string",
+            "email": "string@example.com",
+            "phone": "1234567890",
+        }
+        result.update(profile)
+        return result
+
+
+class UpdateQuieres:
+    def update(self, user_id, profile: ProfileIn):
+        result = {
+            "id": user_id,
+            "user_id": user_id,
+            "role": profile.role,
+            "first_name": profile.first_name,
+            "last_name": profile.last_name,
+            "email": profile.email,
+            "phone": profile.phone,
+        }
+        return result
+
+
+class DeleteQuieres:
+    def delete(self, user_id: int):
+        return True
+
+
+def test_get_all_profiles():
+    app.dependency_overrides[ProfileRepository] = ProfileQuieres
+
+    response = client.get("/api/profile/")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+
+    assert response.json() == [
+        {
+            "id": 1,
+            "user_id": 1,
+            "role": "Admin",
+            "first_name": "string",
+            "last_name": "string",
+            "email": "string@example.com",
+            "phone": "1234567890",
+        },
+        {
+            "id": 2,
+            "user_id": 2,
+            "role": "Supplier",
+            "first_name": "string2",
+            "last_name": "string2",
+            "email": "string2@example.com",
+            "phone": "0987654321",
+        },
+    ]
+
+
+def test_create_profile():
+    app.dependency_overrides[ProfileRepository] = CreateQuieres
+
+    profile = {
+        "user_id": 1,
+        "role": "Admin",
+        "first_name": "string",
+        "last_name": "string",
+        "email": "string@example.com",
+        "phone": "1234567890",
+    }
+
+    expected = {
+        "id": 1,
+        "user_id": 1,
+        "role": "Admin",
+        "first_name": "string",
+        "last_name": "string",
+        "email": "string@example.com",
+        "phone": "1234567890",
+    }
+
+    response = client.post("/api/profile/", json=profile)
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+
+    assert response.json() == expected
+
+
+def test_update_profile():
+
+    app.dependency_overrides[ProfileRepository] = UpdateQuieres
+
+    user_id = 1
+    profile = {
+        "user_id": 1,
+        "role": "Admin",
+        "first_name": "string",
+        "last_name": "string",
+        "email": "string@example.com",
+        "phone": "1234567890",
+    }
+
+    expected = {
+        "id": 1,
+        "user_id": 1,
+        "role": "Admin",
+        "first_name": "string",
+        "last_name": "string",
+        "email": "string@example.com",
+        "phone": "1234567890",
+    }
+
+    response = client.put(f"/api/profile/{user_id}", json=profile)
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+
+    assert response.json() == expected
+
+
+def test_get_one_profile():
+    app.dependency_overrides[ProfileRepository] = ProfileQuieres
+
+    user_id = 1
+
+    expected_product = {
+        "id": 1,
+        "user_id": 1,
+        "role": "Admin",
+        "first_name": "string",
+        "last_name": "string",
+        "email": "string@example.com",
+        "phone": "1234567890",
+    }
+
+    response = client.get(f"/api/profile/{user_id}")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+
+    assert response.json() == expected_product
+
+
+def test_delete_profile():
+    app.dependency_overrides[ProfileRepository] = DeleteQuieres
+
+    user_id = 1
+
+    response = client.delete(f"/api/profile/{user_id}")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+    assert response.json() == True
---
+++ api/tests/test_shops.py
@@ -0,0 +1,170 @@
+from fastapi.testclient import TestClient
+from main import app
+from models.shops import ShopOut, ShopIn
+from queries.shop_database import ShopRepository
+
+
+client = TestClient(app)
+
+
+class ShopsQuieres:
+    def get_all(self):
+        return [
+            ShopOut(
+                shop_id=1,
+                shop_name="string",
+                address="string",
+                phone="string",
+            ),
+            ShopOut(
+                shop_id=2,
+                shop_name="string2",
+                address="string2",
+                phone="string2",
+            ),
+        ]
+
+    def get_one(self, shop_id: int):
+        shops = self.get_all()
+        for shop in shops:
+            if shop.shop_id == shop_id:
+                return shop
+        return None
+
+
+class CreateQuieres:
+    def create(self, shop: ShopIn):
+        result = {
+            "shop_id": 1,
+            "shop_name": "string",
+            "address": "string",
+            "phone": "string",
+        }
+        result.update(shop)
+        return result
+
+
+class UpdateQuieres:
+    def update(self, shop_id, shop: ShopIn):
+        result = {
+            "shop_id": shop_id,
+            "shop_name": shop.shop_name,
+            "address": shop.address,
+            "phone": shop.phone,
+        }
+        return result
+
+
+class DeleteQuieres:
+    def delete(self, shop_id: int):
+        return True
+
+
+def test_get_all_shops():
+    app.dependency_overrides[ShopRepository] = ShopsQuieres
+
+    response = client.get("/api/shops/")
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+
+    assert response.json() == [
+        {
+            "shop_id": 1,
+            "shop_name": "string",
+            "address": "string",
+            "phone": "string",
+        },
+        {
+            "shop_id": 2,
+            "shop_name": "string2",
+            "address": "string2",
+            "phone": "string2",
+        },
+    ]
+
+
+def test_create_shop():
+    app.dependency_overrides[ShopRepository] = CreateQuieres
+
+    shop = {
+        "shop_name": "string",
+        "address": "string",
+        "phone": "string",
+    }
+
+    expected = {
+        "shop_id": 1,
+        "shop_name": "string",
+        "address": "string",
+        "phone": "string",
+    }
+
+    response = client.post("/api/shops/", json=shop)
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+
+    assert response.json() == expected
+
+
+def test_update_shop():
+
+    app.dependency_overrides[ShopRepository] = UpdateQuieres
+
+    shop_id = 1
+    shop = {
+        "shop_name": "string",
+        "address": "string",
+        "phone": "string",
+    }
+
+    expected = {
+        "shop_id": 1,
+        "shop_name": "string",
+        "address": "string",
+        "phone": "string",
+    }
+
+    response = client.put(f"/api/shops/{shop_id}", json=shop)
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+
+    assert response.json() == expected
+
+
+def test_get_one_shop():
+    app.dependency_overrides[ShopRepository] = ShopsQuieres
+
+    shop_id = 1
+
+    expected_shop = {
+        "shop_id": 1,
+        "shop_name": "string",
+        "address": "string",
+        "phone": "string",
+    }
+
+    response = client.get(f"/api/shops/{shop_id}")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+
+    assert response.json() == expected_shop
+
+
+def test_delete_shop():
+    app.dependency_overrides[ShopRepository] = DeleteQuieres
+
+    shop_id = 1
+
+    response = client.delete(f"/api/shops/{shop_id}")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+    assert response.json() == True
