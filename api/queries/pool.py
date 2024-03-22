52cff2aba3180e0bf103f30dc31d5d8551d510cf
Carlos <carlossoftwareengineer@gmail.com>
2024-03-22 12:50:39 -0700

    Fixing pool.py to use DBURL

--- api/queries/pool.py
+++ api/queries/pool.py
@@ -1,30 +1,4 @@
+import os
 from psycopg_pool import ConnectionPool
 
-pool = ConnectionPool(
-    "postgresql://packit:packit@nov-2023-7db-service"
-    ".default.svc.cluster.local/postgres"
-)
-
-
-# import os
-# from psycopg_pool import ConnectionPool
-
-
-# def get_database_url():
-#     """
-#     Get the appropriate database URL based on the environment.
-#     """
-#     environment = os.environ.get("ENVIRONMENT")
-#     if environment == "localhost":
-
-#         return os.environ.get("DATABASE_URL")
-#     else:
-#         # Use server database URL
-#         return (
-#             "postgresql://packit:packit@nov-2023-7db-service"
-#             ".default.svc.cluster.local/postgres"
-#         )
-
-
-# # Create a connection pool using the appropriate database URL
-# pool = ConnectionPool(conninfo=get_database_url())
+pool = ConnectionPool(conninfo=os.environ["DATABASE_URL"])
